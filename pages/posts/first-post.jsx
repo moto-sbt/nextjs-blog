3ab8edfeadca729f440e10ae21cc0354408336b0
m.shibuta <m.shibuta@shibutakitokunoMacBook-Air.local>
2022-07-25 09:48:53 +0900

    feat: ここまで https://nextjs.org/learn/basics/assets-metadata-css/global-styles

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -28,6 +29,6 @@ export default function FirstPost() {
                 width={144}
                 alt="motosbt"
             />
-        </>   
+        </Layout>   
     )
 }
