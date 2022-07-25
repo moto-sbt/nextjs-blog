f9fbb9d965001c02809e7de14c18daac683fd0cc
moto-sbt <moto1220.sbt@outlook.jp>
2022-07-25 23:46:23 +0900

    feat: ここまでできた https://nextjs.org/learn/basics/data-fetching/implement-getstaticprops

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,45 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
-import utilStyle from '../styles/utils.module.css';
+import utileStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyle.headingMd}>
+      <section className={utileStyles.headingMd}>
         <p>Hello, I'm <b>motosbt</b></p>
         <p>You can contact me on <a href="https://twitter.com/moto_sbt">Twitter</a></p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utileStyles.headingMd} ${utileStyles.padding1px}`}>
+        <h2 className={utileStyles.headingLg}>Blog</h2>
+        <ul className={utileStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utileStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
