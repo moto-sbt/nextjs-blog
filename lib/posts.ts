49515f538369c3b6e8c899191b13c78d3f88fe07
m.shibuta <m.shibuta@shibutakitokunoMacBook-Air.local>
2022-08-01 17:37:55 +0900

    feat: 型定義足りてなかったとこ追加

--- lib/posts.ts
+++ lib/posts.ts
@@ -31,7 +31,7 @@ export function getSortedPostsData() {
 
         return {
             id,
-            ...matterResult.data,
+            ...(matterResult.data as { date: string, title: string}),
         };
     });
 
