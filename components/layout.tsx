0b9b2038085c4e2eafeb66bb01a209faaf98f4c0
moto-sbt <moto1220.sbt@outlook.jp>
2022-08-03 10:55:19 +0900

    refactor: import部分を絶対パスにした(#19)

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,7 @@
-import styles from './layout.module.css';
+import styles from 'components/layout.module.css';
 import Head from 'next/head';
 import Image from 'next/image';
-import utilStyles from '../styles/utils.module.css';
+import utilStyles from 'styles/utils.module.css';
 import Link from 'next/link';
 import { FaGithub, FaTwitter } from "react-icons/fa";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Date from '../components/date';
-import Layout, { siteTitle } from '../components/layout';
-import utileStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
+import Date from 'components/date';
+import Layout, { siteTitle } from 'components/layout';
+import utileStyles from 'styles/utils.module.css';
+import { getSortedPostsData } from 'lib/posts';
 import { GetStaticProps } from 'next';
 
 export default function Home({
