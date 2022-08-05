da44ef5188415b607e9194a7445f6eae8fe26c5c
moto-sbt <moto1220.sbt@outlook.jp>
2022-08-05 13:54:35 +0900

    fix: tailwindcss の purge を content に変更(https://tailwindcss.com/docs/upgrade-guide#configure-content-sources )

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  content: [],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
     colors: {
