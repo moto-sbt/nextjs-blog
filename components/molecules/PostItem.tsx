282d7275771e1f7a6e5a8066ced8c4f92a6c7c9a
moto-sbt <moto1220.sbt@outlook.jp>
2022-08-10 16:08:30 +0900

    fix: mapのkey関係の warning を除去(#46)

--- components/molecules/PostItem.tsx
+++ components/molecules/PostItem.tsx
@@ -12,26 +12,24 @@ const PostItem = ({
     date,
     title,
     id,
-    tags
+    tags,
 }: Props) => {
     return (
         <>
-            <li className="my-6" key={id}>
-                <Link href={`/posts/${id}`}>
-                    <a className="leading-7">{title}</a>
+            <Link href={`/posts/${id}`}>
+                <a className="leading-7">{title}</a>
+            </Link>
+            <br />
+            <small className="text-gray">
+                <Date dateString={date} />
+            </small>
+            {tags.map((tag, index) => (
+                <Link href={`/tags/${tag}`} key={index}>
+                    <a className="text-sm text-gray bg-gray-light mx-1 px-1 rounded-md hover:bg-gray-dark hover:no-underline">
+                        <small>{tag}</small>
+                    </a>
                 </Link>
-                <br />
-                <small className="text-gray">
-                    <Date dateString={date} />
-                </small>
-                {tags.map((tag) => (
-                    <Link href={`/tags/${tag}`}>
-                        <a className="text-sm text-gray bg-gray-light mx-1 px-1 rounded-md hover:bg-gray-dark hover:no-underline">
-                            <small>{tag}</small>
-                        </a>
-                    </Link>
-                ))}
-            </li>
+            ))}
         </>
     )
 }
