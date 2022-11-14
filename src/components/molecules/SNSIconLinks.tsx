162598a8189fdf79110c3a51bbc60943725a6f2e
moto-sbt <moto1220.sbt@outlook.jp>
2022-11-14 22:47:40 +0900

    fix:url

--- src/components/molecules/SNSIconLinks.tsx
+++ src/components/molecules/SNSIconLinks.tsx
@@ -10,7 +10,7 @@ const SNSIconLinks = () => {
             <GithubIconLink id="moto-sbt" size={40} repository="nextjs-blog" />
             <TwitterIconLink id="moto_sbt" size={40} />
             <YouTubeIconLink id="UCn5JtdxpDzCRiSM7fdomt9g" size={40} />
-            <TiktokIconLink id="moto-sbt" size={40} />
+            <TiktokIconLink id="moto_sbt" size={40} />
             <InstagramIconLink id="moto_sbt" size={40} />
         </>
     );
